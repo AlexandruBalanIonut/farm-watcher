10e036298e8ef1ce1dbe32467b6155f54f335d87
ProngsDark <alexandru-balan@outlook.com>
2020-11-19 22:03:07 +0200

    Web3 detection is on

--- src/components/high-level/StrategiesOverview.tsx
+++ src/components/high-level/StrategiesOverview.tsx
@@ -1,27 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import Web3 from "web3";
 
 function StrategiesOverview() {
-    const web3 = require('web3');
+    require('web3');
 
+    const [web3Enabled, setWeb3Enabled] = useState(<h1>Unfortunatley we couldn't detect any Ethereum Wallet Extension</h1>);
+    
     useEffect(() => {
         const ethEnabled = () => {
             if ((window as any).ethereum) {
                 (window as any).web3 = new Web3((window as any).ethereum);
                 (window as any).ethereum.enable();
-                return true;
-            }
-            else {
-                return false;
+                setWeb3Enabled(<></>);
             }
         }
 
-        console.log(ethEnabled());
-    })
+        ethEnabled();
+    });
 
     return(
         <div className="w-100">
             <h1 className="mt-6 text-white text-center font-semibold text-2xl sm:text-4xl">Monitor Harvest Vaults</h1>
+            <h1 className="text-white">{web3Enabled}</h1>
         </div>
     );
 }
